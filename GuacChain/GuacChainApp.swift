ccd48f4195143f51c60da67d1a3b2c79172762f4
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-09 04:52:12 -0400

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Lori Rothermel on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
